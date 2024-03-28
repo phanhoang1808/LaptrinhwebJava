7fcd19071f005e81b3f128286690fa8653609bcf
phanhoang1808 <hoangphan18082002@gmail.com>
2024-03-29 01:55:25 +0700

    projectBTL

---
+++ src/java/control/ShowCart.java
@@ -0,0 +1,115 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+
+package control;
+
+import dao.DAO;
+import entity.Good;
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+/**
+ *
+ * @author Admin
+ */
+public class ShowCart extends HttpServlet {
+   
+    /** 
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+                String url = "Cart.jsp";
+
+        HttpSession session = request.getSession();
+        CopyOnWriteArrayList<Good> list = new CopyOnWriteArrayList<>();
+        CopyOnWriteArrayList<Good> listtmp = new CopyOnWriteArrayList<>();
+
+        if (session.getAttribute("List") == null) {
+            String idGood = request.getParameter("pid"); // id cua mon hang duoc chon = id cua san pham duoc chon tren trang home
+            Good selectedGood = new DAO().getGoodFromProductID(idGood);
+            list.add(selectedGood);
+            session.setAttribute("List", list);
+        } else {
+            String idGood = request.getParameter("pid"); // id cua mon hang duoc chon = id cua san pham duoc chon tren trang home
+            Good selectedGood = new DAO().getGoodFromProductID(idGood);
+            listtmp = (CopyOnWriteArrayList<Good>) session.getAttribute("List");
+            boolean existed = false;
+            for (Good g : listtmp) {
+                if (g.getId() == Integer.parseInt(idGood)) {
+                    g.setAmount(g.getAmount() + 1); 
+                    existed = true;
+                    break;
+                }
+            }
+            // neu san pham ch xuat hien trong gia hang thi them vao gio hang
+            if (!existed) {
+                listtmp.add(selectedGood);
+            }
+
+            session.setAttribute("List", listtmp);
+        }
+        double total = 0;
+        for (Good g : listtmp) {
+            total += g.getPrice() * g.getAmount();
+        }
+        double vat = (total) * 0.15;
+        double sum = total + vat;
+        request.setAttribute("total", total); 
+        request.setAttribute("vat", vat);
+        request.setAttribute("sum", sum);
+
+        session.setMaxInactiveInterval(200);
+        request.getRequestDispatcher(url).forward(request, response);
+    } 
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /** 
+     * Handles the HTTP <code>GET</code> method.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        processRequest(request, response);
+    } 
+
+    /** 
+     * Handles the HTTP <code>POST</code> method.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /** 
+     * Returns a short description of the servlet.
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ src/java/entity/Good.java
@@ -0,0 +1,70 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entity;
+
+/**
+ *
+ * @author Admin
+ */
+public class Good {
+    private int id;
+    private String name;
+    private String image;
+    private double price;
+    private int amount;
+
+    public Good() {
+    }
+
+    public Good(int id, String name, String image, double price, int amount) {
+        this.id = id;
+        this.name = name;
+        this.image = image;
+        this.price = price;
+        this.amount = amount;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+    
+    
+}
